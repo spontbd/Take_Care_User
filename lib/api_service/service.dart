e020f83db73321aa8f02832e7ee98cb725c3faca
Sujit Sarkar <snigdho532@gmail.com>
2022-04-04 21:46:48 +0600

    04-04-22

---
+++ lib/api_service/service.dart
@@ -0,0 +1,38 @@
+import 'dart:math';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class NotificationService{
+
+  static final FlutterLocalNotificationsPlugin _flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  static void initialize(){
+    const InitializationSettings initializationSettings =
+         InitializationSettings(
+          android: AndroidInitializationSettings('@mipmap/ic_launcher'));
+    _flutterLocalNotificationsPlugin.initialize(initializationSettings);
+  }
+
+  static void display(RemoteMessage message)async{
+    try{
+      Random random = Random();
+      int id = random.nextInt(1000);
+      const NotificationDetails notificationDetails = NotificationDetails(
+        android: AndroidNotificationDetails(
+          'mychanel',
+          'my chanel',
+          importance: Importance.max,
+          priority: Priority.high
+        ));
+
+      await _flutterLocalNotificationsPlugin.show(
+          id,
+          message.notification!.title,
+          message.notification!.body,
+          notificationDetails);
+    }catch(e){
+      print('Error>>>$e');
+    }
+  }
+}
--- lib/controller/data_controller.dart
+++ lib/controller/data_controller.dart
@@ -1,6 +1,12 @@
+import 'dart:convert';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:get/get.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
 
 class DataController extends GetxController{
   static DataController dc =Get.find();
@@ -17,6 +23,62 @@ class DataController extends GetxController{
     }
     update();
     print('Size: ${size.value}');
+  }
+
+
+  Future<void> generateUserToken()async{
+    try{
+      final FirebaseMessaging fcm = FirebaseMessaging.instance;
+      final fcmToken = await fcm.getToken();
+
+      await FirebaseFirestore.instance.collection('users').doc(fcmToken).set({
+        'token': fcmToken,
+        'name' : '',
+        'id' : '',
+        // 'createdAt': FieldValue.serverTimestamp()
+      },SetOptions(merge: true)).whenComplete((){
+        if (kDebugMode) {print('!!Token Created!!');}
+      });
+    }catch(e){
+      if(kDebugMode){print(e.toString());}
+    }
+  }
+
+  Future<void> sendNotification(String receiver, String token)async{
+    final data = {
+      'click_action': 'FLUTTER_NOTIFICATION_CLICK',
+      'id':'1',
+      'status': 'done',
+      'message':'$receiver you have new message',
+      'sender':'Sujit Sarkar',
+      'receiver': receiver,
+    };
+
+    try{
+      http.Response response = await http.post(
+          Uri.parse('https://fcm.googleapis.com/fcm/send'),
+          headers:<String,String>{
+            'Content-Type': 'application/json',
+            'Authorization': 'key=AAAANQH8hdE:APA91bGQJCiSJClNaFXsgIGNbI3wNM8chjmPXNM5Is0hj_WHlgiyU663TvmpPP4YOwOsVnEnuinjYBY4GZdfNz5nO1w2jSAA5RlgdnkNGPJAf7ZEM_LS_u-pLe5tWntbRffqxmvcoxLB'
+          },
+          body: jsonEncode(<String,dynamic>{
+            'notification': <String,dynamic>{
+              'title': 'Notification from Takecare',
+              'body':'$receiver you have new message'},
+            'priority': 'high',
+            'data': data,
+            'to': token
+          })
+      );
+
+      if(response.statusCode==200){
+        if(kDebugMode){print('Notification sent success');}
+      }else{
+        if(kDebugMode){print('Notification sent Failed!');}
+      }
+    }catch(e){
+      if(kDebugMode){print('Error>>>$e');}
+    }
   }
 
 }
